803d6570d194b4d07259e71d275428d4eb979945
Prince <this.is.kumar.prince@gmail.com>
2021-10-27 08:14:50 +0530

    05-transforms-objects complete

--- src/05-transforms-objects/index.ts
+++ src/05-transforms-objects/index.ts
@@ -9,26 +9,66 @@ const canvas = document.getElementById("myCanvas")!;
 const scene = new THREE.Scene();
 
 /**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
+
+/**
+ * Group
+ */
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -2;
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube1, cube2, cube3);
+
+/**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
 
 //------ Position ------
 // mesh.position.y = 1;
 // mesh.position.x = 1;
 // mesh.position.z = 1;
 // < Position Short Hand >
-mesh.position.set(0.7, -0.6, 1);
+// mesh.position.set(0.7, -0.6, 1);
 
-scene.add(mesh);
+//------ Scale ------
+// mesh.scale.set(2, 0.5, 0.5);
+
+//------ Rotation ------
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+
+// scene.add(mesh);
 
 //------ Distances ------
 // console.log(mesh.position.length());
 // console.log(mesh.position.distanceTo(new THREE.Vector3(0, 0, 0)));
-mesh.position.normalize();
-console.log(mesh.position.length());
+// mesh.position.normalize();
+// console.log(mesh.position.length());
 
 /**
  * Sizes
@@ -42,7 +82,9 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// ----- Position -----
 camera.position.z = 3;
+// camera.lookAt(mesh.position);
 scene.add(camera);
 
 //------ Distances ------
