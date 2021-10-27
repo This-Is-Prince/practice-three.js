5c1dfbc2cdeaed47d44183be09087ad70c8ee55b
Prince <this.is.kumar.prince@gmail.com>
2021-10-27 14:11:44 +0530

    10-debug-ui completed

--- src/10-debug-ui/index.ts
+++ src/10-debug-ui/index.ts
@@ -1,6 +1,14 @@
 import "../style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({ closed: true });
+// gui.hide();
 
 /**
  * Window Events
@@ -22,19 +30,19 @@ window.addEventListener("resize", () => {
 });
 
 // Full screen
-const exitFullScreen = async () => {
-  await document.exitFullscreen();
-};
-const goFulLScreen = async () => {
-  await canvas.requestFullscreen();
-};
-window.addEventListener("dblclick", () => {
-  if (document.fullscreenElement) {
-    exitFullScreen();
-  } else {
-    goFulLScreen();
-  }
-});
+// const exitFullScreen = async () => {
+//   await document.exitFullscreen();
+// };
+// const goFulLScreen = async () => {
+//   await canvas.requestFullscreen();
+// };
+// window.addEventListener("dblclick", () => {
+//   if (document.fullscreenElement) {
+//     exitFullScreen();
+//   } else {
+//     goFulLScreen();
+//   }
+// });
 
 /**
  * Canvas
@@ -47,12 +55,36 @@ const canvas = document.getElementById("myCanvas")!;
 const scene = new THREE.Scene();
 
 /**
+ * Param
+ */
+const parameters = {
+  color: 0xff0000,
+  spin: () => {
+    gsap.to(mesh.rotation, { y: Math.PI * 2 + mesh.rotation.y, duration: 1 });
+  },
+};
+/**
  * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
 const mesh = new THREE.Mesh(geometry, material);
+mesh.visible = true;
 scene.add(mesh);
+
+// Debug
+// gui.add(mesh.position, "x", -3, 3, 0.0001);
+gui.add(mesh.position, "x").min(-3).max(3).step(0.0001).name("mesh position x");
+gui.add(mesh.position, "y").min(-3).max(3).step(0.0001).name("mesh position y");
+gui.add(mesh.position, "z").min(-3).max(3).step(0.0001).name("mesh position z");
+
+gui.add(mesh, "visible");
+// gui.add(material, "wireframe");
+gui.add(mesh.material, "wireframe");
+gui.addColor(parameters, "color").onChange(() => {
+  material.color = new THREE.Color(parameters.color);
+});
+gui.add(parameters, "spin");
 
 /**
  * Sizes
