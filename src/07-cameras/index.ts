f88171c460fadb4cc4d87ca30c792ffac2897fc6
Prince <this.is.kumar.prince@gmail.com>
2021-10-27 11:19:42 +0530

    07-cameras completed

--- src/07-cameras/index.ts
+++ src/07-cameras/index.ts
@@ -1,1 +1,116 @@
-console.log("hi");
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Cursor
+ */
+// const cursor = {
+//   x: 0,
+//   y: 0,
+// };
+
+// window.addEventListener("mousemove", (event) => {
+//   cursor.x = (event.clientX / sizes.width - 0.5) * 1;
+//   cursor.y = -(event.clientY / sizes.height - 0.5) * 1;
+//     console.log(cursor);
+// });
+
+/**
+ * Canvas
+ */
+const canvas = document.getElementById("myCanvas")!;
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+
+// ---- Orthographic Camera
+// let aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+
+// ---- Perspective Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+
+// camera.position.set(2, 2, 2);
+camera.position.z = 2;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+/**
+ * Next Frame
+ */
+
+// Clock
+const clock = new THREE.Clock();
+
+const tick = () => {
+  // Update Controls
+  controls.update();
+
+  // Elapsed Time
+  const elapsedTime = clock.getElapsedTime();
+
+  //   Update Objects
+  //   mesh.rotation.y = elapsedTime;
+
+  //   Update Camera
+  //   camera.position.x = cursor.x * 10;
+  //   camera.position.y = cursor.y * 10;
+
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 3;
+  //   camera.lookAt(mesh.position);
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Running This Function on Next Frame
+  window.requestAnimationFrame(tick);
+};
+tick();
