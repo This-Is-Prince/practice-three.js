6c0454816ce40b0fabb66933610344e498758359
Prince <this.is.kumar.prince@gmail.com>
2021-10-27 07:04:12 +0530

    05-transforms-objects positions done

--- src/05-transforms-objects/index.ts
+++ src/05-transforms-objects/index.ts
@@ -1,1 +1,56 @@
-console.log("hi");
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.getElementById("myCanvas")!;
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+//------ Position ------
+// mesh.position.y = 1;
+// mesh.position.x = 1;
+// mesh.position.z = 1;
+// < Position Short Hand >
+mesh.position.set(0.7, -0.6, 1);
+
+scene.add(mesh);
+
+//------ Distances ------
+// console.log(mesh.position.length());
+// console.log(mesh.position.distanceTo(new THREE.Vector3(0, 0, 0)));
+mesh.position.normalize();
+console.log(mesh.position.length());
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//------ Distances ------
+// console.log(mesh.position.distanceTo(camera.position));
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
