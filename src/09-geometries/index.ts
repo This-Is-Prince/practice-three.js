56dc6e3d394778fb5685e9fb4fc151741dd497e2
Prince <this.is.kumar.prince@gmail.com>
2021-10-27 13:20:26 +0530

    09-geometries completed

--- src/09-geometries/index.ts
+++ src/09-geometries/index.ts
@@ -1,1 +1,101 @@
 import "../style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Window Events
+ */
+window.addEventListener("resize", () => {
+  // Update Sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update Camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update  Renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+/**
+ * Canvas
+ */
+const canvas = document.getElementById("myCanvas")!;
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+
+const geometry = new THREE.BufferGeometry();
+// const vertices = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+// geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+const count = 50;
+const positionsAttribute = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsAttribute[i] = (Math.random() - 0.5) * 3;
+}
+geometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positionsAttribute, 3)
+);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+scene.add(camera);
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+/**
+ * Next Frame
+ */
+const tick = () => {
+  // Update Controls
+  controls.update();
+  // Render
+  renderer.render(scene, camera);
+
+  // Request Animation Frame
+  window.requestAnimationFrame(tick);
+};
+tick();
