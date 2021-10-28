fe7e62d1fd2c3f2616551a0f92f84f317aa277fd
Prince <this.is.kumar.prince@gmail.com>
2021-10-28 18:48:17 +0530

    19-ray-caster completed

--- src/19-ray-caster/index.ts
+++ src/19-ray-caster/index.ts
@@ -57,6 +57,25 @@ object3.position.x = 2;
 scene.add(object1, object2, object3);
 
 /**
+ * Ray Caster
+ */
+
+const raycaster = new THREE.Raycaster();
+// const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// const rayDirection = new THREE.Vector3(10, 0, 0);
+// rayDirection.normalize();
+// raycaster.set(rayOrigin, rayDirection);
+
+// const intersectObject = raycaster.intersectObject(object1);
+// console.log(intersectObject);
+// const intersectObjects = raycaster.intersectObjects([
+//   object1,
+//   object2,
+//   object3,
+// ]);
+// console.log(intersectObjects);
+
+/**
  * Sizes
  */
 const sizes = {
@@ -66,6 +85,21 @@ const sizes = {
 const aspectRatio = () => {
   return sizes.width / sizes.height;
 };
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2(0, 0);
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+});
+window.addEventListener("click", () => {
+  //   console.log("click anywhere");
+  if (currentIntersect) {
+    console.log("click on a sphere");
+  }
+});
 
 /**
  * Camera
@@ -78,6 +112,7 @@ scene.add(camera);
  * Controls
  */
 const controls = new OrbitControls(camera, canvas);
+controls.enabled = false;
 controls.enableDamping = true;
 
 /**
@@ -90,9 +125,49 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Tick
  */
+const clock = new THREE.Clock();
+let currentIntersect = null;
 const tick = () => {
+  // Elapsed Time
+  const elapsedTime = clock.getElapsedTime();
+
   // Update Controls
   controls.update();
+
+  // Animate Objects
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+  //   Cast a ray
+
+  raycaster.setFromCamera(mouse, camera);
+
+  //   const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  //   const rayDirection = new THREE.Vector3(1, 0, 0);
+  //   rayDirection.normalize();
+
+  //   raycaster.set(rayOrigin, rayDirection);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+  for (const object of objectsToTest) {
+    object.material.color.set(0xff0000);
+  }
+  for (const intersect of intersects) {
+    intersect.object.material.color.set(0x0000ff);
+  }
+  if (intersects.length) {
+    if (currentIntersect === null) {
+      console.log("mouse enter");
+    }
+    currentIntersect = intersects[0];
+  } else {
+    if (currentIntersect) {
+      console.log("mouse leave");
+    }
+    currentIntersect = null;
+  }
 
   // Render
   renderer.render(scene, camera);
