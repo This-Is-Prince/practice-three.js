b713792ef1fe959628b4888f8e92f610cc937332
Prince <this.is.kumar.prince@gmail.com>
2021-10-30 18:25:38 +0530

    makeGalaxy fun added

--- src/17-particles/index.ts
+++ src/17-particles/index.ts
@@ -34,6 +34,11 @@ window.addEventListener("resize", () => {
 const scene = new THREE.Scene();
 
 /**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+/**
  * Objects
  */
 
@@ -60,13 +65,95 @@ const makeCircle = (radius: number, thickness: number) => {
     positions[index + 2] = z;
   }
 };
-makeCircle(1, 0.1);
-particlesGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3)
-);
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particles);
+// makeCircle(1, 0);
+const galaxyParameters = {
+  noOfParticles: 100000,
+  noOfSubDivision: 18,
+  radius: 10,
+  height: 0.1,
+  subDivisionThickness: 0.25,
+  bending: 3,
+};
+let particles: THREE.Points;
+const makeGalaxy = () => {
+  positions = new Float32Array(galaxyParameters.noOfParticles * 3);
+  // Remove Previous Particles
+  if (particles !== undefined) {
+    particlesGeometry.dispose();
+    particlesMaterial.dispose();
+    particles.remove();
+  }
+  // Calculate Positions of particles
+  for (let i = 0; i < galaxyParameters.noOfParticles; i++) {
+    let index = i * 3;
+    // Distance From Center Of Galaxy (radius)
+    let distanceFromCenterOfGalaxy = Math.random() * galaxyParameters.radius;
+    // Find How Many SubDivision
+    let whichSubDivision = i % galaxyParameters.noOfSubDivision;
+    // Angle For Each Subdivision
+    let angleOfSubDivision =
+      Math.PI * 2 * (whichSubDivision / galaxyParameters.noOfSubDivision) +
+      (Math.random() - 0.5) * galaxyParameters.subDivisionThickness +
+      Math.pow(distanceFromCenterOfGalaxy, galaxyParameters.bending) * 0.01;
+    // x Position
+    positions[index + 0] =
+      Math.cos(angleOfSubDivision) * distanceFromCenterOfGalaxy;
+    // y Position
+    positions[index + 1] = (Math.random() - 0.5) * galaxyParameters.height;
+    // z Position
+    positions[index + 2] =
+      Math.sin(angleOfSubDivision) * distanceFromCenterOfGalaxy;
+  }
+  // SetAttribute in Buffer Geometry
+  particlesGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3)
+  );
+  // New Particles
+  particles = new THREE.Points(particlesGeometry, particlesMaterial);
+
+  // Adding in Scene
+  scene.add(particles);
+};
+// Making Galaxy
+makeGalaxy();
+// Make Galaxy Debug GUI
+gui
+  .add(galaxyParameters, "noOfParticles")
+  .min(500)
+  .max(100000)
+  .step(100)
+  .onFinishChange(makeGalaxy);
+gui
+  .add(galaxyParameters, "noOfSubDivision")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(makeGalaxy);
+gui
+  .add(galaxyParameters, "radius")
+  .min(1)
+  .max(20)
+  .step(0.0001)
+  .onFinishChange(makeGalaxy);
+gui
+  .add(galaxyParameters, "height")
+  .min(0)
+  .max(1)
+  .step(0.0001)
+  .onFinishChange(makeGalaxy);
+gui
+  .add(galaxyParameters, "subDivisionThickness")
+  .min(0)
+  .max(0.25)
+  .step(0.0001)
+  .onFinishChange(makeGalaxy);
+gui
+  .add(galaxyParameters, "bending")
+  .min(1)
+  .max(5)
+  .step(1)
+  .onFinishChange(makeGalaxy);
 
 /**
  * Sizes
@@ -86,7 +173,7 @@ updateSizes();
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
-camera.position.set(1, 1, 2);
+camera.position.set(0, 10, 0);
 scene.add(camera);
 
 /**
