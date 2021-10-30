dc3f243b2d2dad6bdc7880aeeae249b60ff46854
Prince <this.is.kumar.prince@gmail.com>
2021-10-30 19:47:17 +0530

    stars project started

--- src/17-particles/index.ts
+++ src/17-particles/index.ts
@@ -44,30 +44,38 @@ scene.add(axesHelper);
 
 let particlesMaterial = new THREE.PointsMaterial({
   size: 0.02,
-  vertexColors: true,
+  // vertexColors: true,
 });
 
 let particlesGeometry = new THREE.BufferGeometry();
+let particles: THREE.Points;
 const noOfParticle = 1000;
 let positions = new Float32Array(noOfParticle * 3);
 let colors = new Float32Array(noOfParticle * 3);
 
-const makeCircle = (radius: number, thickness: number) => {
-  for (let i = 0; i < noOfParticle; i++) {
-    let index = i * 3;
-    let rnd = Math.random();
-    let x = Math.sin(rnd * Math.PI * 2);
-    let z = Math.cos(rnd * Math.PI * 2);
+// const makeCircle = (radius: number, thickness: number) => {
+//   for (let i = 0; i < noOfParticle; i++) {
+//     let index = i * 3;
+//     let rnd = Math.random();
+//     let x = Math.sin(rnd * Math.PI * 2);
+//     let z = Math.cos(rnd * Math.PI * 2);
 
-    let rnd1 = Math.random() * thickness;
-    x = x * radius * (rnd1 + 1);
-    z = z * radius * (rnd1 + 1);
-    positions[index + 0] = x;
-    positions[index + 1] = 0;
-    positions[index + 2] = z;
-  }
-};
+//     let rnd1 = Math.random() * thickness;
+//     x = x * radius * (rnd1 + 1);
+//     z = z * radius * (rnd1 + 1);
+//     positions[index + 0] = x;
+//     positions[index + 1] = 0;
+//     positions[index + 2] = z;
+//   }
+// };
 // makeCircle(1, 0);
+// particlesGeometry.setAttribute(
+//   "position",
+//   new THREE.BufferAttribute(positions, 3)
+// );
+// particles = new THREE.Points(particlesGeometry, particlesMaterial);
+// scene.add(particles);
+
 const galaxyParameters = {
   noOfParticles: 100000,
   noOfSubDivision: 18,
@@ -78,7 +86,6 @@ const galaxyParameters = {
   insideColor: 0xff6030,
   outsideColor: 0x1b3984,
 };
-let particles: THREE.Points;
 const makeGalaxy = () => {
   // Remove Previous Particles
   if (particles !== undefined) {
@@ -142,46 +149,83 @@ const makeGalaxy = () => {
   scene.add(particles);
 };
 // Making Galaxy
-makeGalaxy();
+// makeGalaxy();
 // Make Galaxy Debug GUI
-gui
-  .add(galaxyParameters, "noOfParticles")
-  .min(500)
-  .max(100000)
-  .step(100)
-  .onFinishChange(makeGalaxy);
-gui
-  .add(galaxyParameters, "noOfSubDivision")
-  .min(2)
-  .max(20)
-  .step(1)
-  .onFinishChange(makeGalaxy);
-gui
-  .add(galaxyParameters, "radius")
-  .min(1)
-  .max(20)
-  .step(0.0001)
-  .onFinishChange(makeGalaxy);
-gui
-  .add(galaxyParameters, "height")
-  .min(0)
-  .max(1)
-  .step(0.0001)
-  .onFinishChange(makeGalaxy);
-gui
-  .add(galaxyParameters, "subDivisionThickness")
-  .min(0)
-  .max(0.5)
-  .step(0.0001)
-  .onFinishChange(makeGalaxy);
-gui
-  .add(galaxyParameters, "bending")
-  .min(1)
-  .max(5)
-  .step(1)
-  .onFinishChange(makeGalaxy);
-gui.addColor(galaxyParameters, "insideColor").onFinishChange(makeGalaxy);
-gui.addColor(galaxyParameters, "outsideColor").onFinishChange(makeGalaxy);
+// gui
+//   .add(galaxyParameters, "noOfParticles")
+//   .min(500)
+//   .max(100000)
+//   .step(100)
+//   .onFinishChange(makeGalaxy);
+// gui
+//   .add(galaxyParameters, "noOfSubDivision")
+//   .min(2)
+//   .max(20)
+//   .step(1)
+//   .onFinishChange(makeGalaxy);
+// gui
+//   .add(galaxyParameters, "radius")
+//   .min(1)
+//   .max(20)
+//   .step(0.0001)
+//   .onFinishChange(makeGalaxy);
+// gui
+//   .add(galaxyParameters, "height")
+//   .min(0)
+//   .max(1)
+//   .step(0.0001)
+//   .onFinishChange(makeGalaxy);
+// gui
+//   .add(galaxyParameters, "subDivisionThickness")
+//   .min(0)
+//   .max(0.5)
+//   .step(0.0001)
+//   .onFinishChange(makeGalaxy);
+// gui
+//   .add(galaxyParameters, "bending")
+//   .min(1)
+//   .max(5)
+//   .step(1)
+//   .onFinishChange(makeGalaxy);
+// gui.addColor(galaxyParameters, "insideColor").onFinishChange(makeGalaxy);
+// gui.addColor(galaxyParameters, "outsideColor").onFinishChange(makeGalaxy);
+
+/**
+ * Stars
+ */
+const starsParameters = {
+  noOfStars: 1000,
+  maxDistanceFromCenter: 10,
+};
+const makeStars = () => {
+  //Destroy Old Stars
+  if (particles !== undefined) {
+    particlesGeometry.dispose();
+    particlesMaterial.dispose();
+    scene.remove(particles);
+  }
+  positions = new Float32Array(starsParameters.noOfStars * 3);
+  particlesGeometry = new THREE.BufferGeometry();
+
+  for (let i = 0; i < starsParameters.noOfStars; i++) {
+    let index = i * 3;
+    positions[index + 0] =
+      (Math.random() - 0.5) * starsParameters.maxDistanceFromCenter;
+    positions[index + 1] =
+      (Math.random() - 0.5) * starsParameters.maxDistanceFromCenter;
+    positions[index + 2] =
+      (Math.random() - 0.5) * starsParameters.maxDistanceFromCenter;
+  }
+
+  // set attribute
+  particlesGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3)
+  );
+  particles = new THREE.Points(particlesGeometry, particlesMaterial);
+  scene.add(particles);
+};
+makeStars();
 
 /**
  * Sizes
@@ -201,7 +245,8 @@ updateSizes();
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
-camera.position.set(0, 10, 0);
+// camera.position.set(0, 10, 0);
+camera.position.set(0, 0, 5);
 scene.add(camera);
 
 /**
