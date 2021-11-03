e7570b29b8bd775dc74cfdc9d81ab316f841f000
Prince <this.is.kumar.prince@gmail.com>
2021-11-03 10:27:26 +0530

    23-realistic-render started

---
+++ src/23-realistic-render/index.ts
@@ -0,0 +1,97 @@
+import "../style.css";
+import * as THREE from "three";
+import * as dat from "dat.gui";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Canvas
+ */
+const canvas = document.getElementById("myCanvas")!;
+
+/**
+ * Window Events
+ */
+window.addEventListener("resize", () => {
+  // Update Sizes
+  updateSizes();
+
+  // Update Camera
+  camera.aspect = aspectRatio();
+  scene.add(camera);
+
+  // Update Renderer
+  updateRenderer();
+});
+
+/**
+ * Debug GUI
+ */
+const gui = new dat.GUI();
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene();
+
+/**
+ * Test Sphere
+ */
+const testSphere = new THREE.Mesh(
+  new THREE.SphereGeometry(1, 32, 32),
+  new THREE.MeshBasicMaterial()
+);
+scene.add(testSphere);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 0,
+  height: 0,
+};
+const aspectRatio = () => {
+  return sizes.width / sizes.height;
+};
+const updateSizes = () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+};
+updateSizes();
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
+camera.position.set(4, 1, -4);
+scene.add(camera);
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas });
+const updateRenderer = () => {
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+};
+updateRenderer();
+
+/**
+ * Tick
+ */
+const tick = () => {
+  // Update Controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Next Frame
+  window.requestAnimationFrame(tick);
+};
+tick();
