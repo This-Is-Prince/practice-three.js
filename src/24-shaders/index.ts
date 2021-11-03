7f215f14fd948d419420e3b2bf80d5c37845e1c8
Prince <this.is.kumar.prince@gmail.com>
2021-11-03 13:48:05 +0530

    random values send from index.ts

--- src/24-shaders/index.ts
+++ src/24-shaders/index.ts
@@ -39,6 +39,12 @@ const scene = new THREE.Scene();
  */
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+const count = geometry.attributes.position.count;
+const randoms = new Float32Array(count);
+for (let i = 0; i < count; i++) {
+  randoms[i] = Math.random();
+}
+geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
 
 // Material
 // 1.way
