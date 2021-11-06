b4bacd552684f3798cdda06764e4161f65b21b6f
Prince <this.is.kumar.prince@gmail.com>
2021-11-06 19:49:06 +0530

    textures loaded

--- src/16-haunted-house/index.ts
+++ src/16-haunted-house/index.ts
@@ -35,13 +35,66 @@ window.addEventListener("resize", () => {
 const scene = new THREE.Scene();
 
 /**
+ * Loaders
+ */
+const loadingManager = new THREE.LoadingManager();
+const textureLoader = new THREE.TextureLoader(loadingManager);
+
+/**
+ * Textures
+ */
+const doorColorTexture = textureLoader.load("./static/textures/door/color.jpg");
+const doorAlphaTexture = textureLoader.load("./static/textures/door/alpha.jpg");
+const doorNormalTexture = textureLoader.load(
+  "./static/textures/door/normal.jpg"
+);
+const doorHeightTexture = textureLoader.load(
+  "./static/textures/door/height.jpg"
+);
+const doorAmbientOcclusionTexture = textureLoader.load(
+  "./static/textures/door/ambientOcclusion.jpg"
+);
+const doorMetalnessTexture = textureLoader.load(
+  "./static/textures/door/metalness.jpg"
+);
+const doorRoughnessTexture = textureLoader.load(
+  "./static/textures/door/roughness.jpg"
+);
+
+/**
+ * Lights
+ */
+// Ambient Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+// Directional Light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
+/**
+ * Material
+ */
+const floorMaterial = new THREE.MeshStandardMaterial();
+
+/**
  * Objects
  */
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1),
-  new THREE.MeshBasicMaterial()
+// floor
+const floor = new THREE.Mesh(new THREE.PlaneGeometry(20, 20), floorMaterial);
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+// House
+const house = new THREE.Group();
+house.position.y = 2;
+scene.add(house);
+const wall = new THREE.Mesh(
+  new THREE.BoxGeometry(5, 4, 5),
+  new THREE.MeshStandardMaterial()
 );
-scene.add(mesh);
+
+house.add(wall);
 
 /**
  * Sizes
@@ -63,7 +116,8 @@ updateSizes();
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
-camera.position.set(1, 1, 2);
+camera.position.set(1.5, 3.5, 8);
+
 scene.add(camera);
 
 /**
@@ -87,6 +141,7 @@ updateRenderer();
  */
 // Clock
 const clock = new THREE.Clock();
+
 // Tick
 const tick = () => {
   // Update Controls
@@ -102,3 +157,12 @@ const tick = () => {
   window.requestAnimationFrame(tick);
 };
 tick();
+
+/**
+ * ALL Debugging
+ */
+// Main Camera
+const cameraGUI = gui.addFolder("Main Camera");
+cameraGUI.add(camera.position, "x").min(-10).max(10).step(0.0001);
+cameraGUI.add(camera.position, "y").min(-10).max(10).step(0.0001);
+cameraGUI.add(camera.position, "z").min(-10).max(10).step(0.0001);
