3235643f3dfdd2fcd2c5288d1cc112222b03797d
Prince <this.is.kumar.prince@gmail.com>
2021-11-10 07:07:42 +0530

    sRGBEncoding added

--- src/23-realistic-render/index.ts
+++ src/23-realistic-render/index.ts
@@ -1,7 +1,15 @@
 import "../style.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Loaders
+ */
+const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
 
 /**
  * Canvas
@@ -27,6 +35,9 @@ window.addEventListener("resize", () => {
  * Debug GUI
  */
 const gui = new dat.GUI();
+const debugObject = {
+  envMapIntensity: 5,
+};
 
 /**
  * Scene
@@ -38,9 +49,102 @@ const scene = new THREE.Scene();
  */
 const testSphere = new THREE.Mesh(
   new THREE.SphereBufferGeometry(1, 32, 32),
-  new THREE.MeshBasicMaterial()
+  new THREE.MeshStandardMaterial()
 );
 scene.add(testSphere);
+
+/**
+ * Update All Materials
+ */
+const updateALlMaterials = () => {
+  scene.traverse((child) => {
+    if (
+      child instanceof THREE.Mesh &&
+      child.material instanceof THREE.MeshStandardMaterial
+    ) {
+      // child.material.envMap = environmentMap;
+      child.material.envMapIntensity = debugObject.envMapIntensity;
+    }
+  });
+};
+
+/**
+ * Environment Map
+ */
+const environmentMap = cubeTextureLoader.load([
+  "./static/textures/environmentMaps/0/px.jpg",
+  "./static/textures/environmentMaps/0/nx.jpg",
+  "./static/textures/environmentMaps/0/py.jpg",
+  "./static/textures/environmentMaps/0/ny.jpg",
+  "./static/textures/environmentMaps/0/pz.jpg",
+  "./static/textures/environmentMaps/0/nz.jpg",
+]);
+environmentMap.encoding = THREE.sRGBEncoding;
+
+scene.background = environmentMap;
+scene.environment = environmentMap;
+
+gui
+  .add(debugObject, "envMapIntensity")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .onChange(updateALlMaterials);
+
+/**
+ * Models
+ */
+
+gltfLoader.load(
+  "./static/models/FlightHelmet/glTF/FlightHelmet.gltf",
+  (gltf) => {
+    gltf.scene.scale.set(10, 10, 10);
+    gltf.scene.position.set(0, -4, 0);
+    gltf.scene.rotation.y = Math.PI * 0.5;
+    scene.add(gltf.scene);
+
+    updateALlMaterials();
+
+    gui
+      .add(gltf.scene.rotation, "y")
+      .min(-Math.PI)
+      .max(Math.PI)
+      .step(0.001)
+      .name("flightHelmetRotationY");
+  }
+);
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+directionalLight.position.set(0.25, 3, -2.25);
+scene.add(directionalLight);
+
+gui
+  .add(directionalLight, "intensity")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("lightIntensity");
+gui
+  .add(directionalLight.position, "x")
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .name("lightX");
+gui
+  .add(directionalLight.position, "y")
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .name("lightY");
+gui
+  .add(directionalLight.position, "z")
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .name("lightZ");
 
 /**
  * Sizes
@@ -80,6 +184,8 @@ const updateRenderer = () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 };
 updateRenderer();
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
 
 /**
  * Tick
