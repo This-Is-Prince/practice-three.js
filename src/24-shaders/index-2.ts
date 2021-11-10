28e2dfaf6fcb56d0f0ab68779150284abad841b1
Prince <this.is.kumar.prince@gmail.com>
2021-11-10 08:43:32 +0530

    24-shaders practice started

--- src/24-shaders/index-2.ts
+++ src/24-shaders/index-2.ts
@@ -2,6 +2,8 @@ import "../style.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import vertexShader from "./shaders/first/vertex.vs.glsl?raw";
+import fragmentShader from "./shaders/first/fragment.fs.glsl?raw";
 
 /**
  * Canvas
@@ -37,6 +39,69 @@ const scene = new THREE.Scene();
  */
 const textureLoader = new THREE.TextureLoader();
 const flagTexture = textureLoader.load("./static/textures/flag-indian.png");
+
+/**
+ * Objects
+ */
+// Geometry
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+const count = geometry.attributes.position.count;
+const randoms = new Float32Array(count);
+for (let i = 0; i < count; i++) {
+  randoms[i] = Math.random();
+}
+geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
+
+// Material
+// 1.way
+// const material = new THREE.RawShaderMaterial({
+//   vertexShader: `
+// uniform mat4 projectionMatrix;
+// uniform mat4 viewMatrix;
+// uniform mat4 modelMatrix;
+
+// attribute vec3 position;
+
+// void main(){
+//   gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position,1.0);
+// }
+// `,
+//   fragmentShader: `
+// precision mediump float;
+
+// void main(){
+//   gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+// }
+// `,
+// });
+
+// 2.way
+const material = new THREE.RawShaderMaterial({
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    uFrequency: { value: new THREE.Vector2(10, 5) },
+    uTime: { value: 0 },
+    uColor: { value: new THREE.Color(0xff0000) },
+    uTexture: { value: flagTexture },
+  },
+});
+
+gui
+  .add(material.uniforms.uFrequency.value, "x")
+  .min(0)
+  .max(20)
+  .step(0.01)
+  .name("frequencyX");
+gui
+  .add(material.uniforms.uFrequency.value, "y")
+  .min(0)
+  .max(20)
+  .step(0.01)
+  .name("frequencyY");
+const mesh = new THREE.Mesh(geometry, material);
+mesh.scale.y = 2 / 3;
+scene.add(mesh);
 
 /**
  * Sizes
@@ -88,6 +153,9 @@ const tick = () => {
 
   // Elapsed Time
   const elapsedTime = clock.getElapsedTime();
+
+  // Update uTime
+  material.uniforms.uTime.value = elapsedTime;
 
   // Render
   renderer.render(scene, camera);
