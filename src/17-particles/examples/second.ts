98675e5e3a382ae027c5f338b757c67d7a99fbdc
Prince <this.is.kumar.prince@gmail.com>
2021-11-12 08:42:11 +0530

    second shape added

--- src/17-particles/examples/second.ts
+++ src/17-particles/examples/second.ts
@@ -45,8 +45,10 @@ const textureLoader = new THREE.TextureLoader();
  * Geometry
  */
 let obj = {
-  count: 100000,
+  count: 10000,
   size: 0.02,
+  radiusOffset: 1,
+  radius: 2,
 };
 
 let geometry: THREE.BufferGeometry;
@@ -70,10 +72,17 @@ const make = () => {
 
   for (let i = 0; i < obj.count; i++) {
     let i3 = i * 3;
+    let angle = Math.random() * 2 * Math.PI;
+    let radius = 1 + Math.random();
+    let side = 4;
+    let x = (Math.random() - 0.5) * side;
+    let y = (Math.random() - 0.5) * side;
 
-    positions[i3 + 0] = (Math.random() - 0.5) * 10;
-    positions[i3 + 1] = (Math.random() - 0.5) * 10;
-    positions[i3 + 2] = (Math.random() - 0.5) * 10;
+    positions[i3 + 0] =
+      x >= -radius && x <= radius ? Math.sin(angle) * radius : x;
+    positions[i3 + 1] =
+      y >= -radius && y <= radius ? Math.cos(angle) * radius : y;
+    positions[i3 + 2] = 0;
   }
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
 
@@ -107,7 +116,7 @@ const aspectRatio = () => {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 1000);
-camera.position.z = 3;
+camera.position.z = 7;
 scene.add(camera);
 
 /**
