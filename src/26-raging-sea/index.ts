009f3c4c234d5318c5afedd5d6d2b19e92a1b463
Prince <this.is.kumar.prince@gmail.com>
2021-11-17 05:42:32 +0530

    raging-sea complete

--- src/26-raging-sea/index.ts
+++ src/26-raging-sea/index.ts
@@ -14,6 +14,10 @@ const canvas = document.getElementById("myCanvas")!;
  * Debug GUI
  */
 const gui = new dat.GUI({ width: 340 });
+const debugObject = {
+  depthColor: 0x186691,
+  surfaceColor: 0x9bd8ff,
+};
 
 /**
  * Window Events
@@ -38,7 +42,7 @@ const scene = new THREE.Scene();
 /**
  * Geometry
  */
-const geometry = new THREE.PlaneGeometry(2, 2, 128, 128);
+const geometry = new THREE.PlaneGeometry(2, 2, 512, 512);
 
 /**
  * Material
@@ -46,7 +50,106 @@ const geometry = new THREE.PlaneGeometry(2, 2, 128, 128);
 const material = new THREE.ShaderMaterial({
   fragmentShader,
   vertexShader,
+  uniforms: {
+    uTime: { value: 0 },
+    uBigWavesElevation: { value: 0.2 },
+    uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+    uBigWavesSpeed: { value: 0.75 },
+
+    // Small Waves
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallWavesIterations: { value: 4 },
+
+    // Color
+    uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+    uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+    uColorOffset: { value: 0.08 },
+    uColorMultiplier: { value: 5.0 },
+  },
 });
+
+/**
+ * Debug
+ */
+gui
+  .add(material.uniforms.uBigWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uBigWavesElevation");
+gui
+  .add(material.uniforms.uBigWavesFrequency.value, "x")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequencyX");
+gui
+  .add(material.uniforms.uBigWavesFrequency.value, "y")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequencyY");
+gui
+  .add(material.uniforms.uBigWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uBigWavesSpeed");
+
+// Colors debug
+gui
+  .addColor(debugObject, "depthColor")
+  .name("depthColor")
+  .onChange(() => {
+    material.uniforms.uDepthColor.value.set(debugObject.depthColor);
+  });
+gui
+  .addColor(debugObject, "surfaceColor")
+  .name("surfaceColor")
+  .onChange(() => {
+    material.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
+  });
+
+gui
+  .add(material.uniforms.uColorOffset, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uColorOffset");
+gui
+  .add(material.uniforms.uColorMultiplier, "value")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uColorMultiplier");
+
+// Small Waves
+gui
+  .add(material.uniforms.uSmallWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uSmallWavesElevation");
+gui
+  .add(material.uniforms.uSmallWavesFrequency, "value")
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name("uSmallWavesFrequency");
+gui
+  .add(material.uniforms.uSmallWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uSmallWavesSpeed");
+gui
+  .add(material.uniforms.uSmallWavesIterations, "value")
+  .min(0)
+  .max(5)
+  .step(1)
+  .name("uSmallWavesIterations");
 
 /**
  * Mesh
@@ -105,6 +208,9 @@ const tick = () => {
 
   // Update Time
   const elapsedTime = clock.getElapsedTime();
+
+  // Update Material
+  material.uniforms.uTime.value = elapsedTime;
 
   // Render
   renderer.render(scene, camera);
