083bef6ff41aeeb60a04bac976fdc16e7690d1c3
Prince <this.is.kumar.prince@gmail.com>
2021-11-27 06:19:25 +0530

    particles practice completed

--- src/17-particles/index.ts
+++ src/17-particles/index.ts
@@ -33,31 +33,48 @@ const particleTexture = texturesLoader.load(
 // Geometry
 const particlesGeometry = new THREE.BufferGeometry();
 
-const count = 5000;
+const count = 20000;
 const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
 for (let i = 0; i < count; i++) {
   let i3 = i * 3;
-  positions[i3 + 0] = (Math.random() - 0.5) * 3;
-  positions[i3 + 1] = (Math.random() - 0.5) * 3;
-  positions[i3 + 2] = (Math.random() - 0.5) * 3;
+  positions[i3 + 0] = (Math.random() - 0.5) * 10;
+  positions[i3 + 1] = (Math.random() - 0.5) * 10;
+  positions[i3 + 2] = (Math.random() - 0.5) * 10;
+
+  colors[i3 + 0] = Math.random();
+  colors[i3 + 1] = Math.random();
+  colors[i3 + 3] = Math.random();
 }
 particlesGeometry.setAttribute(
   "position",
   new THREE.BufferAttribute(positions, 3)
 );
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
 // Materials
 const particlesMaterial = new THREE.PointsMaterial({});
 particlesMaterial.size = 0.1;
 particlesMaterial.sizeAttenuation = true;
-particlesMaterial.color = new THREE.Color(0xff88cc);
+// particlesMaterial.color = new THREE.Color(0xff88cc);
 particlesMaterial.transparent = true;
 particlesMaterial.alphaMap = particleTexture;
 // particlesMaterial.alphaTest = 0.001;
+// particlesMaterial.depthTest = false;
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending;
+particlesMaterial.vertexColors = true;
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 scene.add(particles);
+
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial()
+// );
+// scene.add(cube);
 
 /**
  * Sizes
@@ -91,7 +108,7 @@ window.addEventListener("resize", () => {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
-camera.position.z = 1;
+camera.position.z = 2;
 scene.add(camera);
 
 /**
@@ -113,7 +130,22 @@ updateRenderer();
 /**
  * Animations
  */
+const clock = new THREE.Clock();
+
 const tick = () => {
+  // Elapsed Time
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update Particles
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+    let x = particlesGeometry.attributes.position.array[i3 + 0];
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + x
+    );
+  }
+  particlesGeometry.attributes.position.needsUpdate = true;
+
   // Update Controls
   controls.update();
 
