23ce68f6b6fc804bc0126e56737025d4e6483844
Prince <this.is.kumar.prince@gmail.com>
2021-11-28 10:30:42 +0530

    fence models added in three.js

--- src/practice-project/fence/index.ts
+++ src/practice-project/fence/index.ts
@@ -1,2 +1,155 @@
-import * as THREE from "three";
 import "../../style.css";
+import * as dat from "dat.gui";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+/**
+ *  Canvas
+ */
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+
+/**
+ *  Debug GUI
+ */
+const gui = new dat.GUI();
+const parameters = {
+  boltColor: 0xcccccc,
+  woodColor: 0xe23e21,
+};
+
+/**
+ *  Scene
+ */
+const scene = new THREE.Scene();
+
+/**
+ * Models
+ */
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("./static/draco/");
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+gltfLoader.load("./static/models/fence/fence.glb", (gltf) => {
+  const fenceWood = gltf.scene.children[0].children[0];
+  const fenceBolt = gltf.scene.children[0].children[1];
+  gui.addColor(parameters, "boltColor").onChange(() => {
+    if (fenceBolt instanceof THREE.Mesh) {
+      if (fenceBolt.material instanceof THREE.MeshStandardMaterial) {
+        fenceBolt.material.color.set(parameters.boltColor);
+      }
+    }
+  });
+  gui.addColor(parameters, "woodColor").onChange(() => {
+    if (fenceWood instanceof THREE.Mesh) {
+      if (fenceWood.material instanceof THREE.MeshStandardMaterial) {
+        fenceWood.material.color.set(parameters.woodColor);
+      }
+    }
+  });
+
+  scene.add(fenceBolt);
+  scene.add(fenceWood);
+});
+
+/**
+ * Lights
+ */
+// Ambient Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+// Directional Light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.near = 5;
+directionalLight.shadow.camera.far = 10;
+directionalLight.shadow.camera.left = -2;
+directionalLight.shadow.camera.right = 2;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.bottom = -2;
+directionalLight.position.set(0, 5, 5);
+scene.add(directionalLight);
+const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(cameraHelper);
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(5, 5),
+  new THREE.MeshStandardMaterial()
+);
+floor.rotation.x = -Math.PI * 0.5;
+floor.position.y = -0.6;
+scene.add(floor);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 0,
+  height: 0,
+};
+const updateSizes = () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+};
+updateSizes();
+const aspectRatio = () => {
+  return sizes.width / sizes.height;
+};
+
+window.addEventListener("resize", () => {
+  // Update Sizes
+  updateSizes();
+
+  // Update Camera
+  camera.aspect = aspectRatio();
+  camera.updateProjectionMatrix();
+
+  // Update Renderer
+  updateRenderer();
+});
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas });
+const updateRenderer = () => {
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+};
+updateRenderer();
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+/**
+ * Animations
+ */
+const tick = () => {
+  // Update Controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Next Frame
+  window.requestAnimationFrame(tick);
+};
+tick();
