c919c88b1a0dda862ba93a4f7f059281dab1a881
Prince <this.is.kumar.prince@gmail.com>
2021-12-09 16:44:13 +0530

    font animation added

--- src/practice-project/opening_website/index.ts
+++ src/practice-project/opening_website/index.ts
@@ -2,6 +2,8 @@ import "../../style.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Font, FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import vertexShader from "./shaders/index-1/vertex.vs.glsl?raw";
 import fragmentShader from "./shaders/index-1/fragment.fs.glsl?raw";
 
@@ -29,21 +31,34 @@ window.addEventListener("resize", () => {
   // Update Renderer
   updateRenderer();
 });
+
+/**
+ * Font Loader
+ */
+let material: THREE.RawShaderMaterial;
+const fontLoader = new FontLoader();
+fontLoader.load("./static/fonts/helvetiker_regular.typeface.json", (font) => {
+  const geometry = new TextGeometry("Prince", {
+    font,
+    size: 0.4,
+    height: 0.2,
+  });
+  geometry.center();
+  material = new THREE.RawShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      uTime: { value: 0 },
+    },
+  });
+  const text = new THREE.Mesh(geometry, material);
+  scene.add(text);
+});
+
 /**
  * Scene
  */
 const scene = new THREE.Scene();
-
-/**
- * Plane
- */
-const planeGeometry = new THREE.PlaneGeometry(1, 1);
-const planeMaterial = new THREE.RawShaderMaterial({
-  vertexShader,
-  fragmentShader,
-});
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-scene.add(plane);
 
 /**
  * Sizes
@@ -65,7 +80,7 @@ updateSizes();
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
-camera.position.set(0, 0, 1);
+camera.position.set(0, 0, 2);
 scene.add(camera);
 
 /**
@@ -92,6 +107,11 @@ const clock = new THREE.Clock();
 const tick = () => {
   // Elapsed Time
   const elapsedTime = clock.getElapsedTime();
+
+  // Update Material
+  if (material) {
+    material.uniforms.uTime.value = elapsedTime;
+  }
 
   // Update Controls
   controls.update();
--- src/practice-project/opening_website/shaders/index-1/vertex.vs.glsl
+++ src/practice-project/opening_website/shaders/index-1/vertex.vs.glsl
@@ -2,13 +2,17 @@ uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
+uniform float uTime;
+
 
 attribute vec3 position;
 
 void main(){
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position.x,abs(sin(uTime)) * position.y, position.z, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition; 
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
-    gl_Position = projectedPosition;
+    vec4 pos = vec4(projectedPosition);
+
+    gl_Position = pos;
 }
