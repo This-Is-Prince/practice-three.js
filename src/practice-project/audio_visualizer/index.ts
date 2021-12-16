f3c4e65fd092f69721b32249df8a77321495581d
Prince <this.is.kumar.prince@gmail.com>
2021-12-16 06:49:54 +0530

    audio_visualizer started

---
+++ src/practice-project/audio_visualizer/index.ts
@@ -0,0 +1,106 @@
+import "../../style.css";
+import * as THREE from "three";
+import * as dat from "dat.gui";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Canvas
+ */
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+
+/**
+ * Window Events
+ */
+window.addEventListener("resize", () => {
+  // Update sizes
+  updateSizes();
+
+  // Update Camera
+  camera.aspect = aspectRatio();
+  camera.updateProjectionMatrix();
+
+  // Update Renderer
+  updateRenderer();
+});
+
+/**
+ * GUI
+ */
+const gui = new dat.GUI();
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene();
+
+/**
+ * Audio
+ */
+const context = new window.AudioContext();
+const oscillator = context.createOscillator();
+oscillator.type = "sine";
+oscillator.frequency.value = 440;
+oscillator.connect(context.destination);
+// oscillator.start();
+console.log();
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 0,
+  height: 0,
+};
+const updateSizes = () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+};
+updateSizes();
+const aspectRatio = () => {
+  return sizes.width / sizes.height;
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+const updateRenderer = () => {
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+};
+updateRenderer();
+
+/**
+ * Animations
+ */
+// Clock
+const clock = new THREE.Clock();
+
+// Tick
+const tick = () => {
+  // Elapsed Time
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update Controls
+  controls.update();
+
+  // Renderer
+  renderer.render(scene, camera);
+
+  // Next frame
+  window.requestAnimationFrame(tick);
+};
+tick();
