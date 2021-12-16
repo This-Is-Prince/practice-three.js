175c56e888678843b4991764374b4870606774eb
Prince <this.is.kumar.prince@gmail.com>
2021-12-16 20:25:13 +0530

    web_audio_api included in 3d

--- src/practice-project/web_audio_api/index.ts
+++ src/practice-project/web_audio_api/index.ts
@@ -1,123 +1,174 @@
+import "../../style.css";
+import * as THREE from "three";
+import * as dat from "dat.gui";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Canvas
+ */
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+
+/**
+ * Window Events
+ */
+// Window resize
+window.addEventListener("resize", () => {
+  // Update sizes
+  updateSizes();
+
+  // Update Camera
+  camera.aspect = aspectRatio();
+  camera.updateProjectionMatrix();
+
+  // Update Renderer
+  updateRenderer();
+});
+
+// Window Mouse Events
+const mouse = new THREE.Vector2(-2, -2);
+window.addEventListener("mousemove", (e) => {
+  mouse.x = (e.clientX / sizes.width - 0.5) * 2;
+  mouse.y = -((e.clientY / sizes.height - 0.5) * 2);
+});
+
+/**
+ * Audio
+ */
 // fetch audio file
-const audio = new Audio("../../../static/sounds/The_Rains_of_Castamere.m4a");
-
-// select our play button
-const playButton = document.getElementById(
-  "play-pause-btn"
-) as HTMLButtonElement;
-
-// select our volume input
-const volumeControl = document.getElementById("volume") as HTMLInputElement;
-const stereoPannerControl = document.getElementById(
-  "stereoPanner"
-) as HTMLInputElement;
-const pannerPositionXControl = document.getElementById(
-  "panner_positionX"
-) as HTMLInputElement;
-const pannerPositionYControl = document.getElementById(
-  "panner_positionY"
-) as HTMLInputElement;
-const pannerPositionZControl = document.getElementById(
-  "panner_positionZ"
-) as HTMLInputElement;
+const audio = new Audio("./static/sounds/The_Rains_of_Castamere.m4a");
 
 // AudioContext
 const audioContext = new AudioContext();
 const track = audioContext.createMediaElementSource(audio);
 
-// GainNode
-const gainNode = audioContext.createGain();
-
-// StereoPannerNode
-// --Using factory method
-// const pannerNode = audioContext.createStereoPanner();
-// --Using Constructor
-const stereoPannerNode = new StereoPannerNode(audioContext, { pan: 0 });
-
 // PannerNode
 const pannerNode = audioContext.createPanner();
-// pannerNode.panningModel = "HRTF";
-// pannerNode.distanceModel = "inverse";
-// pannerNode.refDistance = 1;
-// pannerNode.maxDistance = 10000;
-// pannerNode.rolloffFactor = 1;
-// pannerNode.coneInnerAngle = 360;
-// pannerNode.coneOuterAngle = 0;
-// pannerNode.coneOuterGain = 0;
-console.log(pannerNode);
 
-track.connect(gainNode).connect(pannerNode).connect(audioContext.destination);
+track.connect(pannerNode).connect(audioContext.destination);
 
-// When audio ended playButton state changes
-audio.addEventListener(
-  "ended",
-  () => {
-    playButton.dataset.playing = "false";
-    playButton.innerHTML = "Play";
-  },
-  false
-);
-
-// On range input volume change
-volumeControl.addEventListener(
-  "input",
-  function () {
-    gainNode.gain.value = Number(this.value);
-  },
-  false
-);
-
-// On range input stereoPanner change
-stereoPannerControl.addEventListener(
-  "input",
-  function () {
-    // stereoPannerNode.pan.value = Number(this.value);
-  },
-  false
-);
-
-// On range input panner_positionX change
-pannerPositionXControl.addEventListener(
-  "input",
-  function () {
-    pannerNode.positionX.value = Number(this.value);
-  },
-  false
-);
-pannerPositionYControl.addEventListener(
-  "input",
-  function () {
-    pannerNode.positionY.value = Number(this.value);
-  },
-  false
-);
-pannerPositionZControl.addEventListener(
-  "input",
-  function () {
-    pannerNode.positionZ.value = Number(this.value);
-  },
-  false
-);
-
-// When playButton clicked we play the audio
-playButton.addEventListener(
+window.addEventListener(
   "click",
-  function () {
-    // check if context is in suspended state (autoplay policy)
-    if (audioContext.state === "suspended") {
-      audioContext.resume();
-    }
-
-    // play or pause track depending on state
-    if (this.dataset.playing === "false") {
-      audio.play();
-      this.innerHTML = "Pause";
-      this.dataset.playing = "true";
-    } else if (this.dataset.playing === "true") {
-      audio.pause();
-      this.innerHTML = "Play";
-      this.dataset.playing = "false";
+  () => {
+    if (isMouseOver) {
+      isClicked = !isClicked;
+      if (audioContext.state === "suspended") {
+        audioContext.resume();
+      }
+      if (isClicked) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
     }
   },
   false
 );
+
+/**
+ * GUI
+ */
+const gui = new dat.GUI();
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xffff00 })
+);
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 0,
+  height: 0,
+};
+const updateSizes = () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+};
+updateSizes();
+const aspectRatio = () => {
+  return sizes.width / sizes.height;
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, aspectRatio(), 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+const updateRenderer = () => {
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+};
+updateRenderer();
+
+/**
+ * RayCasting
+ */
+const rayCasting = new THREE.Raycaster();
+
+/**
+ * Animations
+ */
+// prev mesh object
+let isMouseOver = false;
+let isClicked = false;
+
+// Clock
+const clock = new THREE.Clock();
+
+// Tick
+const tick = () => {
+  // Elapsed Time
+  const elapsedTime = clock.getElapsedTime();
+
+  // Cast A Ray
+  rayCasting.setFromCamera(mouse, camera);
+
+  const obj = rayCasting.intersectObjects([mesh]);
+  if (obj.length > 0) {
+    isMouseOver = true;
+  } else {
+    isMouseOver = false;
+  }
+
+  // Update Panner
+  // OrientationX
+  pannerNode.orientationX.value = camera.rotation.x;
+  pannerNode.orientationY.value = camera.rotation.y;
+  pannerNode.orientationZ.value = camera.rotation.z;
+  // OrientationY
+  pannerNode.positionX.value = camera.position.x;
+  pannerNode.positionY.value = camera.position.y;
+  pannerNode.positionZ.value = camera.position.z;
+
+  // Update Controls
+  controls.update();
+
+  // Renderer
+  renderer.render(scene, camera);
+
+  // Next frame
+  window.requestAnimationFrame(tick);
+};
+tick();
