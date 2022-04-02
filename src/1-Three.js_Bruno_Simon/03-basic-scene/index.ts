4b47416beb56bd3293c4ecaf4b935ba17c232fef
Prince <this.is.kumar.prince@gmail.com>
2022-04-02 18:55:04 +0530

    basic scene revised

--- src/1-Three.js_Bruno_Simon/03-basic-scene/index.ts
+++ src/1-Three.js_Bruno_Simon/03-basic-scene/index.ts
@@ -1,36 +1,26 @@
 import * as THREE from "three";
 
-// Canvas
+// Canvas Elements
 const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 
 // Scene
 const scene = new THREE.Scene();
 
-// Geometry
+// Red Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-// Material
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-});
-
-// Mesh
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 // Sizes
 const sizes = {
   width: 800,
   height: 600,
 };
-const aspectRatio = () => {
-  return sizes.width / sizes.height;
-};
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, aspectRatio());
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-scene.add(camera);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
